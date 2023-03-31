6c71b2c4b5ed01433b3902533107af5274397fc6
student <you@example.com>
2023-03-31 20:12:22 +0300

    functions commit

---
+++ math.py
@@ -0,0 +1,20 @@
+
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract(a,b):
+    subtracted_result = a-b
+    return subtracted_result
+
+def divide(a,b):
+    divided_result = a/b
+    return divided_result
+
+def multiply(a,b):
+    multiplied_result = a*b
+    return multiplied_result
+
+def remainder(a,b):
+    divided_result = a%b
+    return divided_result
